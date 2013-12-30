1a9c51c3cd0360788ca5cade3162bf83be2d2888
Justin Kohlhepp <justin.kohlhepp@thehartford.com>
2013-12-30 16:22:02 -0500

    TODO for ELMAH

--- source/Sordid.Web/Controllers/CharacterController.cs
+++ source/Sordid.Web/Controllers/CharacterController.cs
@@ -25,6 +25,7 @@ namespace Sordid.Web.Controllers
             // TODO: Logs not getting written because there is no log4net config
             // TODO: Test that Glimpse log4net extension is working
             // TODO: Why isn't the Glimpse EF6 extension working?  Issue some SQL here to test it out.
+            // TODO: Add ELMAH
             _logger.Info("Rendering new character view...");
 
             return View();
