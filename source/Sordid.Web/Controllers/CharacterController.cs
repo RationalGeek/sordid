6455cf43c6bb266e4db8ff6f540f2359b2b66511
Justin Kohlhepp <justin.kohlhepp@thehartford.com>
2013-12-30 16:21:44 -0500

    Added support libraries: Ninject, Glimpse, log4net, etc.

--- source/Sordid.Web/Controllers/CharacterController.cs
+++ source/Sordid.Web/Controllers/CharacterController.cs
@@ -1,9 +1,17 @@
-﻿using System.Web.Mvc;
+﻿using Ninject.Extensions.Logging;
+using System.Web.Mvc;
 
 namespace Sordid.Web.Controllers
 {
     public class CharacterController : Controller
     {
+        private ILogger _logger;
+
+        public CharacterController(ILogger logger)
+        {
+            _logger = logger;
+        }
+
         // GET: /Character/
         public ActionResult Index()
         {
@@ -13,6 +21,12 @@ namespace Sordid.Web.Controllers
         // GET: /Character/New
         public ActionResult New()
         {
+            // TODO: Delete this test log entry
+            // TODO: Logs not getting written because there is no log4net config
+            // TODO: Test that Glimpse log4net extension is working
+            // TODO: Why isn't the Glimpse EF6 extension working?  Issue some SQL here to test it out.
+            _logger.Info("Rendering new character view...");
+
             return View();
         }
 	}
