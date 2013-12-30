22ab95378d14e579772bfc0ed8557f1c78f7eb83
Justin Kohlhepp <justin.kohlhepp@thehartford.com>
2013-12-30 16:00:52 -0500

    Added initial cut of character controller

---
+++ source/Sordid.Web/Controllers/CharacterController.cs
@@ -0,0 +1,19 @@
+﻿using System.Web.Mvc;
+
+namespace Sordid.Web.Controllers
+{
+    public class CharacterController : Controller
+    {
+        // GET: /Character/
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: /Character/New
+        public ActionResult New()
+        {
+            return View();
+        }
+	}
+}
