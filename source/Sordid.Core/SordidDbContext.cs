4df5dc045bec27a39351c1239ac0c4fc2be6b67a
Justin Kohlhepp <justin.kohlhepp@thehartford.com>
2013-12-30 14:42:42 -0500

    Moved security model classes to Core, plus other minor changes

---
+++ source/Sordid.Core/SordidDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using Sordid.Core.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sordid.Core
+{
+    public class SordidDbContext : IdentityDbContext<ApplicationUser>
+    {
+    }
+}
--- source/Sordid.Web/Controllers/AccountController.cs
+++ source/Sordid.Web/Controllers/AccountController.cs
@@ -9,6 +9,8 @@ using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
 using Microsoft.Owin.Security;
 using Sordid.Web.Models;
+using Sordid.Core.Model;
+using Sordid.Core;
 
 namespace Sordid.Web.Controllers
 {
@@ -16,7 +18,7 @@ namespace Sordid.Web.Controllers
     public class AccountController : Controller
     {
         public AccountController()
-            : this(new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(new ApplicationDbContext())))
+            : this(new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(new SordidDbContext())))
         {
         }
 
