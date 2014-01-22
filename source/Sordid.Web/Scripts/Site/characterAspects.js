d6d1ddf13a35dfdceaa033e1961467f5b66bad03
RationalGeek <jkohlhepp@gmail.com>
2014-01-22 12:31:55 -0500

    Fixed bug wth Aspects accordion

--- source/Sordid.Web/Scripts/Site/characterAspects.js
+++ source/Sordid.Web/Scripts/Site/characterAspects.js
@@ -1,2 +1,12 @@
 ﻿define('sordid-characterAspects', ['jquery', 'knockout'], function ($, ko) {
+
+    // The accordion sort-of works without this, but opening one section
+    // doesn't collapse the other section unless this is done
+
+    ko.bindingHandlers.collapseAspect = {
+        init: function (element, valueAccessor, allBindingsAccessor, viewModel) {
+            $(element).collapse({ parent: '#aspectAccordion', toggle: false });
+        }
+    };
+
 });
