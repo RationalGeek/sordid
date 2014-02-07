9752b23d9c29d54285169ef3909164123e34e7bc
RationalGeek <jkohlhepp@gmail.com>
2014-02-07 08:02:31 -0500

    Roundtripping of notes to database

--- source/Sordid.Web/Scripts/Site/characterNotes.js
+++ source/Sordid.Web/Scripts/Site/characterNotes.js
@@ -1,12 +1,24 @@
 ﻿define('sordid-characterNotes', ['jquery'], function ($) {
-    $(document).ready(function () {
-        $('#notesEditor').wysihtml5({
-            color: true,
-            size: 'xs'
-        });
-    });
+    // http://stackoverflow.com/questions/16877259/knockout-js-binding-handler-for-wysihtml5
+    ko.bindingHandlers.wysihtml5 = {
+        init: function (element, valueAccessor, allBindingsAccessor, viewModel) {
+            var control = $(element).wysihtml5({
+                "events": {
+                    "change": function () {
+                        var observable = valueAccessor();
+                        observable(control.getValue());
+                    },
+                "size": 'xs'
+                }
+            }).data("wysihtml5").editor;
+        },
+        update: function (element, valueAccessor, allBindingsAccessor, viewModel) {
+            var content = valueAccessor();
+
+            if (content != undefined) {
+                var control = $(element).data("wysihtml5").editor;
+                control.setValue(content());
+            }
+        }
+    };
 });
-
-
-
-
