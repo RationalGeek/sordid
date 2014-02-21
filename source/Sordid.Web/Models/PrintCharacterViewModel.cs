b0d27b2615ef0c6ab3900a48cffe9aa43dd442cd
RationalGeek <jkohlhepp@gmail.com>
2014-02-21 08:36:32 -0500

    Print layout for skills and aspects

--- source/Sordid.Web/Models/PrintCharacterViewModel.cs
+++ source/Sordid.Web/Models/PrintCharacterViewModel.cs
@@ -5,5 +5,37 @@ namespace Sordid.Web.Models
     public class PrintCharacterViewModel
     {
         public Character Character { get; set; }
+
+        public string TranslateAspectTitle(string aspectTitle)
+        {
+            var ret = aspectTitle;
+            if (ret != "High Concept" && ret != "Trouble")
+            {
+                ret = "Other Aspects";
+            }
+            return ret;
+        }
+
+        // TODO: This should be reused in the manage UI where we also translate this - should it be in DB?
+        public string TranslateSkillRank(int rank)
+        {
+            switch(rank)
+            {
+                case 0:
+                    return "Mediocre";
+                case 1:
+                    return "Average";
+                case 2:
+                    return "Fair";
+                case 3:
+                    return "Good";
+                case 4:
+                    return "Great";
+                case 5:
+                    return "Superb";
+            }
+
+            return "WTF?";
+        }
     }
 }
