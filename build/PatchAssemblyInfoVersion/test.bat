4505e4585089dec0edecf8070000d36e22ce7c16
RationalGeek <jkohlhepp@gmail.com>
2014-02-23 10:00:59 -0500

    Finished assembly patch script

--- build/PatchAssemblyInfoVersion/test.bat
+++ build/PatchAssemblyInfoVersion/test.bat
@@ -1,1 +1,3 @@
-"C:\Program Files (x86)\MSBuild\12.0\Bin\msbuild.exe" PatchAssemblyInfoVersion.msbuild
+"C:\Program Files (x86)\MSBuild\12.0\Bin\msbuild.exe" PatchAssemblyInfoVersion.msbuild /p:AssemblyVersionMajor=42 /p:AssemblyVersionMinor=42 /p:AssemblyVersionBuild=42
+
+PAUSE
